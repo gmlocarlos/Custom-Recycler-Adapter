e12e590c3c41be15bc769704bd0be8800b341e07
Carlos Pérez <carlosperezsarmiento@gmail.com>
2020-11-22 18:44:54 -0500

    usage example

--- app/src/main/java/com/gmlo/libraryproject/MainActivity.java
+++ app/src/main/java/com/gmlo/libraryproject/MainActivity.java
@@ -1,12 +1,39 @@
 package com.gmlo.libraryproject;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import android.os.Bundle;
+import android.widget.Toast;
+import com.gmlo.cra.CustomRecyclerAdapter;
+import com.gmlo.libraryproject.databinding.ActivityMainBinding;
+import com.gmlo.libraryproject.databinding.ItemRecyclerBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        binding.recyclerView.setLayoutManager(new LinearLayoutManager(this, RecyclerView.VERTICAL,false));
+        CustomRecyclerAdapter<User, ItemRecyclerBinding> recyclerAdapter = new com.gmlo.cra.CustomRecyclerAdapter<User, ItemRecyclerBinding>() {
+            @Override
+            protected void onBindData(User user, int i, ItemRecyclerBinding itemRecyclerBinding) {
+                itemRecyclerBinding.tvName.setText(user.getName());
+                itemRecyclerBinding.tvAge.setText(String.valueOf(user.getAge()));
+            }
+            @Override
+            protected int getLayoutResId() {
+                return R.layout.item_recycler;
+            }
+            @Override
+            protected void onItemClick(User user, int i) {
+                Toast.makeText(MainActivity.this,"row clicked",Toast.LENGTH_LONG).show();
+            }
+        };
+        recyclerAdapter.setList(User.getSampleUsers());
+        binding.recyclerView.setAdapter(recyclerAdapter);
     }
 }
--- app/src/main/java/com/gmlo/libraryproject/User.java
+++ app/src/main/java/com/gmlo/libraryproject/User.java
@@ -1,4 +1,46 @@
 package com.gmlo.libraryproject;
+import java.util.ArrayList;
+import java.util.List;
 
 public class User {
+    private int userId;
+    private String name;
+    private int age;
+
+    public User(int userId,String name, int age){
+        this.userId = userId;
+        this.name = name;
+        this.age = age;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public static List<User> getSampleUsers(){
+        List<User> listSample = new ArrayList<>();
+        listSample.add(new User(1,"Juan Perez",18));
+        listSample.add(new User(2,"Carlos Sarmiento",32));
+        return listSample;
+    }
 }
